c3d07e419ee8abda355ce9e787d3b5dcc921635a
aileot <46470475+aileot@users.noreply.github.com>
2025-01-09 23:30:31 +0900

    feat(preset): make presets read-only

--- fnl/ex-colors/presets/init.lua
+++ fnl/ex-colors/presets/init.lua
@@ -1,4 +1,19 @@
 -- NOTE: This file will be copied into lua/ by make.
-return {
+local M = {
   relinker = require("ex-colors.presets.relinker"),
 }
+
+local mt = {
+  __index = function(_, k)
+    return error("undefined preset: " .. k)
+  end,
+  __newindex = function()
+    error("presets are read-only")
+  end,
+}
+
+for _, v in pairs(M) do
+  setmetatable(v, mt)
+end
+
+return setmetatable(M, mt)
