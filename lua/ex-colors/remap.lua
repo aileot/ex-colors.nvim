993560c64b24d01c30b482fef507dda2422f890d
aileot <46470475+aileot@users.noreply.github.com>
2025-01-09 22:48:45 +0900

    feat(relinker): set `false` to `relinker` to `as is`

--- lua/ex-colors/remap.lua
+++ lua/ex-colors/remap.lua
@@ -18,32 +18,40 @@ local function undefined_highlight_3f(hl_name)
   end
 end
 local function relink_map_recursively(hl_name, hl_map)
-  local relinker = config.relinker
+  local relinker
+  local or_6_ = config.relinker
+  if not or_6_ then
+    local function _7_(_241)
+      return _241
+    end
+    or_6_ = _7_
+  end
+  relinker = or_6_
   local discard_marker = false
-  local _6_ = hl_map.link
-  if (_6_ == nil) then
+  local _8_ = hl_map.link
+  if (_8_ == nil) then
     return hl_map
-  elseif (nil ~= _6_) then
-    local linked = _6_
-    local _7_ = relinker(linked)
-    if (_7_ == discard_marker) then
+  elseif (nil ~= _8_) then
+    local linked = _8_
+    local _9_ = relinker(linked)
+    if (_9_ == discard_marker) then
       return nil
-    elseif (_7_ == linked) then
+    elseif (_9_ == linked) then
       if not undefined_highlight_3f(linked) then
         return hl_map
       else
         return nil
       end
-    elseif (_7_ == hl_name) then
+    elseif (_9_ == hl_name) then
       local hl_opts = {name = linked}
       local deeper_map = vim.api.nvim_get_hl(0, hl_opts)
       return relink_map_recursively(hl_name, deeper_map)
-    elseif (nil ~= _7_) then
-      local relinked = _7_
+    elseif (nil ~= _9_) then
+      local relinked = _9_
       hl_map.link = relinked
       undefined_highlight_3f(relinked)
       return relink_map_recursively(hl_name, hl_map)
-    elseif (_7_ == nil) then
+    elseif (_9_ == nil) then
       return error(("relinker must return a value; make it return `false` explicitly to discard the hl-group " .. linked))
     else
       return nil
@@ -55,7 +63,15 @@ end
 local function remap_hl_opts(hl_name)
   local keep_link_3f = true
   local omit_default_3f = config.omit_default
-  local relink = config.relinker
+  local relink
+  local or_13_ = config.relinker
+  if not or_13_ then
+    local function _14_(_241)
+      return _241
+    end
+    or_13_ = _14_
+  end
+  relink = or_13_
   local discard_marker = false
   local hl_opts = {name = hl_name, link = keep_link_3f}
   local hl_map = vim.api.nvim_get_hl(0, hl_opts)
@@ -63,28 +79,28 @@ local function remap_hl_opts(hl_name)
     hl_map.default = nil
   else
   end
-  local _12_ = relink(hl_name)
-  if (_12_ == discard_marker) then
+  local _16_ = relink(hl_name)
+  if (_16_ == discard_marker) then
     return nil
-  elseif (_12_ == hl_map.link) then
+  elseif (_16_ == hl_map.link) then
     return nil
-  elseif (nil ~= _12_) then
-    local new_name = _12_
+  elseif (nil ~= _16_) then
+    local new_name = _16_
     undefined_highlight_3f(new_name)
-    local _13_ = relink_map_recursively(new_name, hl_map)
-    if (nil ~= _13_) then
-      local new_map = _13_
-      local _14_ = new_map.link
-      if ((_14_ == new_name) or (_14_ == hl_name)) then
+    local _17_ = relink_map_recursively(new_name, hl_map)
+    if (nil ~= _17_) then
+      local new_map = _17_
+      local _18_ = new_map.link
+      if ((_18_ == new_name) or (_18_ == hl_name)) then
         return nil
       else
-        local _ = _14_
+        local _ = _18_
         return new_name, new_map
       end
     else
       return nil
     end
-  elseif (_12_ == nil) then
+  elseif (_16_ == nil) then
     return error(("relinker must return a value; make it return `false` explicitly to discard the hl-group " .. hl_name))
   else
     return nil
