b980d15caf2787653774cf116e29b7de364177cc
aileot <46470475+aileot@users.noreply.github.com>
2025-01-11 16:26:51 +0900

    refactor(utils): let `new_addable` set default table on creation

--- lua/ex-colors/utils.lua
+++ lua/ex-colors/utils.lua
@@ -2,13 +2,13 @@
 local M = {}
 
 --- Create a new table which supports addition via `t + t2`.
-function M.new_addable()
+function M.new_addable(tbl)
   local mt = {
     __add = function(self, right)
       return vim.list_extend(vim.list_slice(self), right)
     end,
   }
-  return setmetatable({}, {
+  return setmetatable(tbl or {}, {
     __newindex = function(t, k, v)
       rawset(t, k, setmetatable(v, mt))
     end,
